80171bd55d0a710595c81b0c1b81366f76d48f7b
Dipesh Mittal <dipesh@drdroid.io>
2024-04-19 15:06:42 +0530

    Removed superuser setup

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -12,7 +12,7 @@ import { PostHogProvider } from 'posthog-js/react';
 import { GlobalSnackbar } from './components/common/GlobalSnackbar/index.jsx';
 import Loading from './components/common/Loading/index.tsx';
 
-if (process.env.POSTHOG_DISABLED !== 'true') {
+if (process.env.POSTHOG_ENABLED == 'true') {
   posthog.init('phc_DakJVaJiJMjyu764IBSgH2A4OPV57Fu8H7I8XPE09iM', {
     api_host: 'https://pumpkins.drdroid.io',
     ui_host: 'https://app.posthog.com'
