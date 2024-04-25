08b96aa597884d6cfd6dad644851b007d809b763
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-04-25 13:10:17 +0530

    add update and all

--- web/src/constants/api/workflows.ts
+++ web/src/constants/api/workflows.ts
@@ -2,4 +2,5 @@
 
 export const GET_WORKFLOWS = "/executor/workflows/get";
 export const CREATE_WORKFLOW = "/executor/workflows/create";
+export const UPDATE_WORKFLOW = "/executor/workflows/update";
 export const DELETE_WORKFLOW = "/executor/workflows/update";
