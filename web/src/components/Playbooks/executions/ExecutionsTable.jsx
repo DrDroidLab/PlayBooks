58dffb813da61f69ce91857cff3550bb29b3e765
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-04-25 17:14:58 +0530

    colors and update playbook things

--- web/src/components/Playbooks/executions/ExecutionsTable.jsx
+++ web/src/components/Playbooks/executions/ExecutionsTable.jsx
@@ -11,6 +11,31 @@ import NoExistingPlaybook from "./NoExistingExecution.jsx";
 import { renderTimestamp } from "../../../utils/DateUtils.js";
 
 const ExecutionsTableRender = ({ data }) => {
+  const handleStatus = (status) => {
+    let className = "border p-1 text-sm bg-gray-50 w-fit rounded ";
+    switch (status) {
+      case "CREATED":
+        className += "bg-gray-200";
+        break;
+
+      case "RUNNING":
+        className += "bg-blue-200";
+        break;
+
+      case "FINISHED":
+        className += "bg-green-200";
+        break;
+
+      case "FAILED":
+        className += "bg-red-200";
+        break;
+
+      default:
+        break;
+    }
+    return <div className={className}>{status}</div>;
+  };
+
   return (
     <>
       <Table stickyHeader>
@@ -37,7 +62,7 @@ const ExecutionsTableRender = ({ data }) => {
                 </Link>
               </TableCell>
               <TableCell component="th" scope="row">
-                {item.status}
+                {handleStatus(item.status)}
               </TableCell>
               <TableCell component="td" scope="row">
                 {item.created_by}
