60435cf7a481c64bd103028fddf4e5cb3667ca48
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-05-06 16:19:24 +0530

    fix bugs

--- web/src/components/Playbooks/create/CustomNode.jsx
+++ web/src/components/Playbooks/create/CustomNode.jsx
@@ -51,7 +51,7 @@ export default function CustomNode({ data }) {
             alt="logo"
           />
         )}
-        <p className="text-lg font-bold text-center z-10 break-word">
+        <p className="text-lg font-bold text-center z-10 break-word line-clamp-4">
           {data?.step?.description ||
             data?.step?.selectedSource ||
             `Step - ${data?.index + 1}`}
--- web/src/components/Playbooks/create/ParentNode.jsx
+++ web/src/components/Playbooks/create/ParentNode.jsx
@@ -8,7 +8,7 @@ export default function ParentNode() {
   return (
     <div className="px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-400 w-[200px] h-48">
       <div className="flex flex-col">
-        <p className="text-lg font-bold z-10 mt-5">
+        <p className="text-lg font-bold z-10 mt-5 line-clamp-4">
           {playbook.name || "Untitled Playbook"}
         </p>
         {/* <p className="text-sm text-gray-500 z-10 mt-5 italic">
