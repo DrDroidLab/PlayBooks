d266186ae5818c826afaa78828aa2dace2dfda0f
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-05-08 16:24:09 +0530

    remove log

--- web/src/utils/parser/playbook/stepToTasks.ts
+++ web/src/utils/parser/playbook/stepToTasks.ts
@@ -3,7 +3,5 @@ import { handleStepSourceInjector } from "./handleStepSourceInjector.ts";
 
 export default function stepToTasks(step: Step): PlaybookTask[] {
   const tasks: PlaybookTask[] = handleStepSourceInjector(step);
-
-  console.log("tasks", tasks);
   return tasks;
 }
