1b595ba2bb60e1046e3216dcc038ac5be9c4b70f
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-05-09 15:58:51 +0530

    replace on crearte

--- web/src/components/Playbooks/create/StepActions.jsx
+++ web/src/components/Playbooks/create/StepActions.jsx
@@ -40,7 +40,7 @@ function StepActions() {
 
     try {
       const response = await triggerCreatePlaybook(playbookObj).unwrap();
-      navigate(`/playbooks/edit/${response.playbook?.id}`);
+      navigate(`/playbooks/edit/${response.playbook?.id}`, { replace: true });
     } catch (e) {
       console.error(e);
     }
