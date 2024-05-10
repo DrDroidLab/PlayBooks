cf9c0f09e4176ae7e2aa9129c5b181cca8f33539
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-05-10 16:55:36 +0530

    bash output height

--- web/src/components/Playbooks/PlaybookBashActionOutput.jsx
+++ web/src/components/Playbooks/PlaybookBashActionOutput.jsx
@@ -1,9 +1,9 @@
 function PlaybookBashActionOutput({ output }) {
   const outputs = output.command_outputs;
   return (
-    <div className="max-w-xl overflow-scroll bg-white p-2 border rounded">
+    <div className="max-w-xl overflow-scroll bg-white p-2 border rounded flex flex-col gap-2">
       {outputs.map((output) => (
-        <p className="text-sm">
+        <p className="text-sm h-48">
           <pre>{output.output}</pre>
         </p>
       ))}
