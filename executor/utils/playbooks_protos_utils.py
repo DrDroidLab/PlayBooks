e41a6cc48c57160fd58918d4c8329954c005f307
Mohit Goyal <mohit.goyal@drdroid.io>
2024-05-10 15:30:51 +0530

    adds proto seriliser for bash action task

--- executor/utils/playbooks_protos_utils.py
+++ executor/utils/playbooks_protos_utils.py
@@ -141,6 +141,13 @@ def get_api_call_task_execution_proto(task) -> PlaybookActionTaskDefinition:
                                         api_call_task=api_call_task_proto)
 
 
+def get_bash_command_task_execution_proto(task) -> PlaybookActionTaskDefinition:
+    bash_command_task = task.get('bash_command_task', {})
+    bash_command_task_proto = dict_to_proto(bash_command_task, PlaybookApiCallTask)
+    return PlaybookActionTaskDefinition(source=PlaybookActionTaskDefinition.Source.BASH,
+                                        bash_command_task=bash_command_task_proto)
+
+
 def get_playbook_task_definition_proto(db_task_definition):
     task_type = db_task_definition.type
     task = db_task_definition.task
@@ -247,6 +254,8 @@ def get_playbook_task_definition_proto(db_task_definition):
         source = task.get('source', None)
         if source == 'API':
             action_task_proto = get_api_call_task_execution_proto(task)
+        elif source == 'BASH':
+            action_task_proto = get_bash_command_task_execution_proto(task)
         else:
             raise ValueError(f"Invalid source: {source}")
         return PlaybookTaskDefinitionProto(
