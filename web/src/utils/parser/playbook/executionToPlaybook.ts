e30b2eed2e6f32978492f1ccfb2048d49d4ce544
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-05-16 15:43:01 +0530

    mimir integration bug and extractor bug fixed

--- web/src/utils/parser/playbook/executionToPlaybook.ts
+++ web/src/utils/parser/playbook/executionToPlaybook.ts
@@ -3,12 +3,12 @@ import { handleStepSourceExtractor } from "./handleStepSourceExtractor.ts";
 
 export const executionToPlaybook = (playbook_execution) => {
   // TODO: Make it better, also extract logs from here
-  const playbook = playbook_execution.playbook;
-  const stepExecutionLogs = playbook_execution.step_execution_logs;
+  const playbook = playbook_execution?.playbook;
+  const stepExecutionLogs = playbook_execution?.step_execution_logs ?? {};
   const list: Step[] = [];
   for (let [i, stepExecutionLog] of stepExecutionLogs.entries()) {
     const step = structuredClone(stepExecutionLog.step);
-    step.tasks = stepExecutionLog.logs?.map((log) => log.task);
+    step.tasks = stepExecutionLog?.logs?.map((log) => log.task);
     let data: any = handleStepSourceExtractor(step);
 
     const stepData: Step = {
