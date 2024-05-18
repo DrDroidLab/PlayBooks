735eff60768fcc3be56eea9ea1235c8895935793
Mohit Goyal <mohit.goyal@drdroid.io>
2024-05-18 11:43:51 +0530

    adds quick fix for contract

--- connectors/assets/views.py
+++ connectors/assets/views.py
@@ -37,12 +37,22 @@ def assets_models_options(request_message: GetConnectorsAssetsModelsOptionsReque
 def assets_models_get(request_message: GetConnectorsAssetsModelsRequest) -> \
         Union[GetConnectorsAssetsModelsResponse, HttpResponse]:
     account: Account = get_request_account()
-    if not request_message.connector_type:
+    if not request_message.connector_type and not request_message.connector_id.value:
         return GetConnectorsAssetsModelsResponse(success=BoolValue(value=False),
                                                  message=Message(title="Invalid Request",
                                                                  description="Missing connector_type"))
     try:
-        account_connector_assets = asset_manager_facade.get_asset_model_values(account, request_message.connector_type,
+        if request_message.connector_id.value:
+            connector_id = request_message.connector_id.value
+            connector = get_db_account_connectors(account, connector_id)
+            if not connector.exists() or not connector:
+                return GetConnectorsAssetsModelsResponse(success=BoolValue(value=False),
+                                                         message=Message(title="Invalid Request",
+                                                                         description="Connector not found"))
+            connector_type = connector.first().connector_type
+        else:
+            connector_type = request_message.connector_type
+        account_connector_assets = asset_manager_facade.get_asset_model_values(account, connector_type,
                                                                                request_message.type,
                                                                                request_message.filters)
     except Exception as err:
