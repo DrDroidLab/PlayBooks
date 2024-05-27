4a77e72f6bd09db81d38bb5740cce48dd8a49454
Dipesh Mittal <dipesh@drdroid.io>
2024-05-27 17:59:29 +0530

    Fixed preconfig postgres PB drop-down selection

--- web/src/utils/extractors/postgres.ts
+++ web/src/utils/extractors/postgres.ts
@@ -1,7 +1,7 @@
 export const extractPostgresTasks = (step: any) => {
   let stepSource = "POSTGRES";
   let modelType = "POSTGRES_QUERY";
-  let selected = "POSTGRES Database";
+  let selected = "POSTGRES Sql Query";
   const tasks = step.tasks;
   const postgresTask = tasks[0].data_fetch_task?.postgres_data_fetch_task;
 
