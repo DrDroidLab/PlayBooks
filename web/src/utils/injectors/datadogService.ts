f6ba86ddef285aef9a358c61e87adb283a403a5b
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-05-30 21:04:54 +0530

    fix datadog bug

--- web/src/utils/injectors/datadogService.ts
+++ web/src/utils/injectors/datadogService.ts
@@ -7,7 +7,7 @@ export const injectDatadogServiceTasks = (
   const tasks = (step.datadogMetric ?? []).map((metric) => ({
     service_name: step.datadogService?.name,
     environment_name: step?.datadogEnvironment ?? "",
-    metric: metric ?? "",
+    metric: metric?.label ?? metric ?? "",
     metric_family: step.datadogMetricFamily ?? "",
     process_function: "timeseries",
   }));
