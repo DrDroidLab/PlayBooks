4cb5d5ddffff0bd2818864393a68a24247be7ba9
Mohit Goyal <mohit.goyal@drdroid.io>
2024-06-04 12:46:43 +0530

    adds missing task type

--- executor/source_task_executors/documentation_task_executor.py
+++ executor/source_task_executors/documentation_task_executor.py
@@ -20,10 +20,24 @@ class DocumentationSourceManager(PlaybookSourceManager):
                 'display_name': 'Write Markdown Documentation',
                 'category': 'Documentation'
             },
+            Documentation.TaskType.IFRAME: {
+                'task_type': 'IFRAME',
+                'executor': self.execute_iframe,
+                'model_types': [],
+                'display_name': 'Embed an IFrame',
+                'category': 'Documentation'
+            },
         }
 
     def execute_markdown(self, time_range: TimeRange, global_variable_set: Dict,
                          doc_task: Documentation, doc_connector_proto: ConnectorProto) -> PlaybookTaskResult:
+        try:
+            return PlaybookTaskResult(source=self.source)
+        except Exception as e:
+            raise Exception(f"Error while executing API call task: {e}")
+
+    def execute_iframe(self, time_range: TimeRange, global_variable_set: Dict,
+                       doc_task: Documentation, doc_connector_proto: ConnectorProto) -> PlaybookTaskResult:
         try:
             return PlaybookTaskResult(source=self.source)
         except Exception as e:
