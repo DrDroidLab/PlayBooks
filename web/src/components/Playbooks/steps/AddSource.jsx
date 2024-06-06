86433c1156bb122467deeb5c8a2581f83106eec1
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-06-06 16:57:04 +0530

    fix azure logs issue, do not show conenctor on api, markdown and iframe

--- web/src/components/Playbooks/steps/AddSource.jsx
+++ web/src/components/Playbooks/steps/AddSource.jsx
@@ -1,8 +1,11 @@
 import SelectTaskType from "./SelectTaskType.jsx";
 import SelectSource from "./SelectSource.jsx";
 import SelectConnectorOption from "./SelectConnectorOption.jsx";
+import useCurrentStep from "../../../hooks/useCurrentStep.ts";
+import { unsupportedConnctorTypeOptions } from "../../../utils/unsupportedConnctorTypeOptions.ts";
 
 function AddSource({ index }) {
+  const [step] = useCurrentStep(index);
   return (
     <div className="flex flex-col gap-2">
       <div className="relative flex mt-2 gap-2">
@@ -11,9 +14,11 @@ function AddSource({ index }) {
           <SelectTaskType index={index} />
         </div>
       </div>
-      <div className="flex gap-2 items-center">
-        <SelectConnectorOption index={index} />
-      </div>
+      {!unsupportedConnctorTypeOptions.includes(step.taskType) && (
+        <div className="flex gap-2 items-center">
+          <SelectConnectorOption index={index} />
+        </div>
+      )}
     </div>
   );
 }
