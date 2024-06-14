b994a819071afcd306103d954e9f130deab1ca62
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-06-14 14:25:38 +0530

    fix bug

--- web/src/components/Workflows/create/NotificationDetails.jsx
+++ web/src/components/Workflows/create/NotificationDetails.jsx
@@ -49,7 +49,7 @@ function NotificationDetails() {
               currentWorkflow.notification === option.id
                 ? "!bg-white !text-violet-500 border-violet-500"
                 : "text-gray-500 bg-gray-50 border-gray-200"
-            } ${index === options.length - 1 ? "rounded-r" : ""} ${
+            } ${index === options?.length - 1 ? "rounded-r" : ""} ${
               index === 0 ? "rounded-l" : ""
             } p-2 text-sm hover:bg-gray-100 cursor-pointer transition-all border`}>
             {option.label}
