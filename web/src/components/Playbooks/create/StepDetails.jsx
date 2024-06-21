15bf068dd79288173c2dcf25f58d5eb796b5f7e3
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-06-21 13:57:04 +0530

    reset scroll whenever step is changed

--- web/src/components/Playbooks/create/StepDetails.jsx
+++ web/src/components/Playbooks/create/StepDetails.jsx
@@ -3,10 +3,12 @@ import useIsPrefetched from "../../../hooks/useIsPrefetched.ts";
 import Step from "../steps/Step.jsx";
 import useCurrentStep from "../../../hooks/useCurrentStep.ts";
 import { updateCardById } from "../../../utils/execution/updateCardById.ts";
+import { useEffect, useRef } from "react";
 
 function StepDetails() {
   const [step, currentStepId] = useCurrentStep();
   const isPrefetched = useIsPrefetched();
+  const stepRef = useRef(null);
 
   const handleUpdateStepName = (e) => {
     const val = e.target.value;
@@ -15,10 +17,14 @@ function StepDetails() {
       updateCardById("userEnteredDescription", true, currentStepId);
   };
 
+  useEffect(() => {
+    stepRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+  }, [stepRef, currentStepId]);
+
   if (!Object.keys(step).length === 0) return <>No Step Found</>;
 
   return (
-    <div className="p-2 min-h-screen mb-16">
+    <div ref={stepRef} className="p-2 min-h-screen mb-16">
       <h2 className="font-bold mb-2 flex items-center gap-2 justify-between mr-2">
         Title {step?.outputLoading && <CircularProgress size={20} />}
       </h2>
