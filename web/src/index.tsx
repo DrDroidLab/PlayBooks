af835ef5d47f69ae7cf8e4992c3bf9e01d4ef750
Dipesh Mittal <dipesh@drdroid.io>
2024-06-27 17:48:51 +0530

    Trying a fix in posthog events

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import config from "./config";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { TimeRangeProvider } from "./context/TimeRangeProvider";
 import { Provider } from "react-redux";
@@ -12,7 +13,10 @@ import { GlobalSnackbar } from "./components/common/GlobalSnackbar/index.jsx";
 import Loading from "./components/common/Loading/index.tsx";
 import { ReactFlowProvider } from "reactflow";
 
-if (process.env.REACT_APP_POSTHOG_ENABLED === "true") {
+console.log("config", config);
+console.log('REACT_APP_POSTHOG_ENABLED:', window._env_.REACT_APP_POSTHOG_ENABLED);
+
+if (config.posthogEnabled === "true") {
   posthog.init("phc_DakJVaJiJMjyu764IBSgH2A4OPV57Fu8H7I8XPE09iM", {
     api_host: "https://pumpkins.drdroid.io",
     ui_host: "https://app.posthog.com",
