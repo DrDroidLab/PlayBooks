ddaef09c1bc6b11a8e1c623198c6408ae79af847
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-01 16:02:39 +0530

    show details

--- web/src/utils/playbooksData.ts
+++ web/src/utils/playbooksData.ts
@@ -17,9 +17,9 @@ export enum OptionType {
 export const constructBuilder = (id?: string) => {
   const [task, currentStepId] = getCurrentTask(id);
 
-  let ops: any = task.modelOptions;
+  let ops: any = task?.ui_requirement.modelOptions ?? [];
 
-  switch (`${task?.source} ${task?.taskType}`) {
+  switch (`${task?.source} ${task?.[task.source?.toLowerCase()].type}`) {
     case taskTypes.CLICKHOUSE_SQL_QUERY:
       return Builders.clickhouseBuilder(ops?.databases, task);
     case taskTypes.CLOUDWATCH_LOG_GROUP:
@@ -27,7 +27,7 @@ export const constructBuilder = (id?: string) => {
     case taskTypes.CLOUDWATCH_METRIC:
       return Builders.cloudwatchMetricBuilder(ops?.namespaces, task);
     case taskTypes.DATADOG_SERVICE_METRIC_EXECUTION:
-      return Builders.datadogBuilder(ops?.services, task, currentStepId);
+      return Builders.datadogBuilder(ops?.services, task, currentStepId!);
     // case taskTypes.GRAFANA_VPC_PROMQL_METRIC_EXECUTION:
     // case taskTypes.GRAFANA_PROMQL_METRIC_EXECUTION:
     //   return Builders.grafanaBuilder(ops?.dashboards, task);
@@ -35,12 +35,12 @@ export const constructBuilder = (id?: string) => {
       return Builders.grafanaDataSourceBuilder(
         ops?.prometheus_datasources,
         task,
-        currentStepId,
+        currentStepId!,
       );
     case taskTypes.NEW_RELIC_NRQL_METRIC_EXECUTION:
       return Builders.newRelicNRQLBuilder(task, currentStepId);
     case taskTypes.AZURE_FILTER_LOG_EVENTS:
-      return Builders.azureLogsBuilder(ops?.workspaces, task, currentStepId);
+      return Builders.azureLogsBuilder(ops?.workspaces, task, currentStepId!);
     case taskTypes.NEW_RELIC_ENTITY_APPLICATION_GOLDEN_METRIC_EXECUTION:
       return Builders.newRelicEntityApplicationBuilder(
         ops?.application_names,
@@ -51,7 +51,7 @@ export const constructBuilder = (id?: string) => {
       return Builders.newRelicEntityDashboardBuilder(
         ops?.dashboards,
         task,
-        currentStepId,
+        currentStepId!,
       );
     case taskTypes.POSTGRES_SQL_QUERY:
       return Builders.postgresBuilder();
@@ -74,7 +74,7 @@ export const constructBuilder = (id?: string) => {
     case taskTypes.BASH_COMMAND:
       return Builders.bashBuilder(ops?.ssh_servers);
     case taskTypes.DATADOG_QUERY_METRIC_EXECUTION:
-      return Builders.datadogRawQueryBuilder(task, currentStepId);
+      return Builders.datadogRawQueryBuilder(task, currentStepId!);
     case taskTypes.API_HTTP_REQUEST:
       return Builders.apiBuilder(task, currentStepId);
     case taskTypes.SQL_DATABASE_CONNECTION_SQL_QUERY:
