1a7e1649167ffff79c8ec57cd55950d33b8c7ac3
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-02 17:12:13 +0530

    change set nested value

--- web/src/utils/setNestedValue.ts
+++ web/src/utils/setNestedValue.ts
@@ -1,14 +1,31 @@
-export default function setNestedValue(obj, path: string, value: any) {
+export default function setNestedValue(obj: any, path: string, value: any) {
   const keys = path.split(".");
   let current = obj;
 
   for (let i = 0; i < keys.length - 1; i++) {
-    if (!current[keys[i]]) {
-      current[keys[i]] = {};
+    const key = keys[i];
+    const nextKey = keys[i + 1];
+
+    if (!isNaN(parseInt(nextKey))) {
+      // If next key is a numeric value
+      if (!current[key] || !Array.isArray(current[key])) {
+        current[key] = [];
+      }
+    } else {
+      if (!current[key] || typeof current[key] !== "object") {
+        current[key] = {};
+      }
     }
-    current = current[keys[i]];
+
+    current = current[key];
   }
 
-  current[keys[keys.length - 1]] = value;
+  const lastKey = keys[keys.length - 1];
+  if (!isNaN(parseInt(lastKey))) {
+    current[parseInt(lastKey)] = value;
+  } else {
+    current[lastKey] = value;
+  }
+
   return obj;
 }
