80621629d8382d3b0f0b63d82509050058260300
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-03 18:00:43 +0530

    change copy

--- web/src/components/Modals/RecieveUpdatesModal/index.tsx
+++ web/src/components/Modals/RecieveUpdatesModal/index.tsx
@@ -15,21 +15,24 @@ const RecieveUpdatesModal = ({ isOpen, close }) => {
   return (
     <div className="z-50">
       <Overlay close={close} visible={isOpen}>
-        <div className="relative bg-white py-4 px-2 rounded max-w-full w-[300px]">
+        <div className="relative bg-white py-6 px-4 rounded max-w-full w-[300px]">
           <div
             onClick={close}
             className="absolute top-0 right-0 m-2 cursor-pointer">
             <CloseRounded />
           </div>
-          <p className="font-semibold text-sm">Recieve Updates from DrDroid?</p>
+          <p className="font-semibold text-sm">
+            Would you like to receive updates about new features & releases at
+            Playbooks?
+          </p>
 
-          <div className="flex items-center gap-2 my-2">
+          <div className="flex items-center gap-2 mt-4">
             <CustomButton
               onClick={handleYes}
               className="!bg-violet-500 !text-white hover:!text-violet-500 hover:!bg-transparent">
-              Yes
+              Yes Please!
             </CustomButton>
-            <CustomButton onClick={handleNo}>No</CustomButton>
+            <CustomButton onClick={handleNo}>No thanks</CustomButton>
           </div>
         </div>
       </Overlay>
