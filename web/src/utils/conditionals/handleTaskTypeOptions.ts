c81c71b654b2c91644841c0cc08ffc0bcec57f09
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-09 16:02:22 +0530

    only show configured task types

--- web/src/utils/conditionals/handleTaskTypeOptions.ts
+++ web/src/utils/conditionals/handleTaskTypeOptions.ts
@@ -5,9 +5,17 @@ import { Step, Task } from "../../types/index.ts";
 function handleTaskTypeOptions(step: Step | undefined) {
   const currentPlaybook = currentPlaybookSelector(store.getState());
   const tasks = currentPlaybook?.ui_requirement.tasks ?? [];
-  const stepTasks = step?.tasks.map((task: Task | string) =>
-    tasks.find((e) => e.id === (typeof task === "string" ? task : task.id)),
-  );
+  const stepTasks = step?.tasks
+    .map((task: Task | string) => {
+      const taskData = tasks.find(
+        (e) => e.id === (typeof task === "string" ? task : task.id),
+      );
+      if (Object.keys(taskData?.ui_requirement.errors).length === 0) {
+        return undefined;
+      }
+      return taskData;
+    })
+    .filter((task) => task);
   return stepTasks ?? [];
 }
 
