de191694f44d7c3a26326966a128bfdba6ab29be
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-10 16:05:21 +0530

    hide buttons from step details

--- web/src/components/Playbooks/steps/StepDetailsButtons.tsx
+++ web/src/components/Playbooks/steps/StepDetailsButtons.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import DeleteStepButton from "../../Buttons/DeleteStepButton/index.tsx";
 import AddTaskButton from "../../Buttons/AddTaskButton/index.tsx";
 import RunStepButton from "../../Buttons/RunStepButton/index.tsx";
+import useIsPrefetched from "../../../hooks/useIsPrefetched.ts";
 
 function StepDetailsButtons({ id }) {
+  const isPrefetched = useIsPrefetched();
+
   return (
     <div className="flex gap-2 mt-2">
       <RunStepButton id={id} />
-      <AddTaskButton id={id} />
-      <DeleteStepButton id={id} />
+      {!isPrefetched && (
+        <>
+          <AddTaskButton id={id} />
+          <DeleteStepButton id={id} />
+        </>
+      )}
     </div>
   );
 }
