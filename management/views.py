d78a51a7799f456c951d1ad914cf1d4186ebd200
Dipesh Mittal <dipesh@drdroid.io>
2024-07-10 12:30:34 +0530

    Converted github calls to dedicated central backend call

--- management/views.py
+++ management/views.py
@@ -1,6 +1,7 @@
 from typing import Union
 from django.http import HttpResponse
 import os
+import json
 
 import requests
 
@@ -10,50 +11,35 @@ from protos.accounts.api_pb2 import GetUserRequest, GetVersionInfoResponse
 REPO_OWNER = "DrDroidLab"
 REPO_NAME = "PlayBooks"
 
-def get_tags():
-    url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/tags"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise Exception(f"Failed to fetch tags: {response.status_code}")
-
-def get_commit_date(sha):
-    url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/commits/{sha}"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        commit_data = response.json()
-        commit_date = commit_data['commit']['committer']['date']
-        return commit_date
-    else:
-        raise Exception(f"Failed to fetch commit data: {response.status_code}")
-    
-def get_latest_tag():
-    tags = get_tags()
-    
-    latest_tag = None
-    latest_date = None
-
-    for tag in tags:
-        commit_date = get_commit_date(tag['commit']['sha'])
-        if latest_date is None or commit_date > latest_date:
-            latest_date = commit_date
-            latest_tag = tag['name']
-
-    return latest_tag, latest_date
-
-def get_last_commit_timestamp():
+def get_main_last_commit_timestamp():
     url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/commits/main"
     response = requests.get(url)
     
     if response.status_code == 200:
         commit_data = response.json()
-        commit_timestamp = commit_data['commit']['committer']['date']
-        return commit_timestamp
+        commit_date = int(commit_data['commit']['committer']['date'])
+        return commit_date
     else:
-        raise Exception(f"Failed to fetch data: {response.status_code}")
+        raise Exception(f"Failed to fetch commit data: {response.status_code}")
+    
+def get_latest_tag_timestamp():
+    latest_tag = None
+    latest_date = None
+
+    url = "https://app.drdroid.io/management/version-info"
+
+    payload = json.dumps({})
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    if response.status_code == 200:
+        data = response.json()
+        latest_tag = data['latest_version']
+        latest_date = int(data['latest_version_timestamp'])
+
+    return latest_tag, latest_date
 
 @web_api(GetUserRequest)
 def version_info(request_message: GetUserRequest) -> Union[GetVersionInfoResponse, HttpResponse]:
@@ -64,18 +50,15 @@ def version_info(request_message: GetUserRequest) -> Union[GetVersionInfoRespons
     upgrade_message = None
     latest_version = None
 
-    print(f"BUILD_TIMESTAMP: {BUILD_TIMESTAMP}")
-    print(f"IMAGE_VERSION: {IMAGE_VERSION}")
-
-    if IMAGE_VERSION is not None:
-        LATEST_TAG, LATEST_TAG_TIMESTAMP = get_latest_tag()
-        if LATEST_TAG_TIMESTAMP > BUILD_TIMESTAMP:
+    if IMAGE_VERSION is not None and BUILD_TIMESTAMP is not None:
+        LATEST_TAG, LATEST_TAG_TIMESTAMP = get_latest_tag_timestamp()
+        if LATEST_TAG_TIMESTAMP > int(BUILD_TIMESTAMP):
             should_upgrade = True
             latest_version = LATEST_TAG
             upgrade_message = f'New Release Available - {LATEST_TAG}'
     elif BUILD_TIMESTAMP is not None:
-        LATEST_TIMESTAMP = get_last_commit_timestamp()
-        if LATEST_TIMESTAMP > BUILD_TIMESTAMP:
+        LATEST_TIMESTAMP = get_main_last_commit_timestamp()
+        if LATEST_TIMESTAMP > int(BUILD_TIMESTAMP):
             should_upgrade = True
             upgrade_message = 'New version available!'
 
