2fd57eb7d7f8c64d70afa62c7758363c4a7c6b16
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-11 17:12:04 +0530

    date picker

--- web/src/components/common/TimeRangeSelector/CustomTimeRangeSection.tsx
+++ web/src/components/common/TimeRangeSelector/CustomTimeRangeSection.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { DatePicker } from "rsuite";
 
 function CustomTimeRangeSection() {
-  return <div className="p-1">CustomTimeRangeSection</div>;
+  return (
+    <div className="p-1">
+      <p className="font-medium text-sm">Absolute Time Range</p>
+      <DatePicker
+        format="dd MMM yyyy hh:mm:ss aa"
+        showMeridian
+        className="z-[1000]"
+        style={{ width: 220 }}
+      />
+    </div>
+  );
 }
 
 export default CustomTimeRangeSection;
