0847b2a75c9ad881cca536374beafa7340f9c0a1
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-17 18:02:05 +0530

    make it more according to backend

--- web/src/components/AddCondition/BashCommandOutput.tsx
+++ web/src/components/AddCondition/BashCommandOutput.tsx
@@ -14,13 +14,14 @@ import { additionalStateSelector } from "../../store/features/drawers/drawersSli
 import useEdgeConditions from "../../hooks/useEdgeConditions.ts";
 import Checkbox from "../common/Checkbox/index.tsx";
 import { bashCommandOutputOptions } from "../../utils/conditionals/typeOptions/index.ts";
-import HandleTypes from "./HandleTypes.tsx";
+import { addConditionToEdgeByIndex } from "../../utils/conditionals/addConditionToEdgeByIndex.ts";
 
 function BashCommandOutput({ rule, condition, conditionIndex }) {
   const { id } = useSelector(additionalStateSelector);
-  const { handleCondition } = useEdgeConditions(id);
+  const { handleCondition, edgeIndex } = useEdgeConditions(id);
+  const keyValue = condition?.type?.toLowerCase();
 
-  const handleChange = (val: string, type: string) => {
+  const handleChange = (val: string | undefined, type: string) => {
     handleCondition(type, val, conditionIndex);
   };
 
@@ -30,9 +31,11 @@ function BashCommandOutput({ rule, condition, conditionIndex }) {
         <SelectComponent
           error={undefined}
           data={bashCommandOutputOptions}
-          selected={condition.conditionType}
-          placeholder={`Select Condition`}
-          onSelectionChange={(id: string) => handleChange(id, "conditionType")}
+          selected={rule.type}
+          placeholder={`Select Type`}
+          onSelectionChange={(id: string) =>
+            handleChange(id, `${keyValue}.type`)
+          }
         />
       </div>
 
@@ -40,8 +43,10 @@ function BashCommandOutput({ rule, condition, conditionIndex }) {
         <ValueComponent
           error={undefined}
           valueType={"STRING"}
-          onValueChange={(val: string) => handleChange(val, "pattern")}
-          value={condition.pattern}
+          onValueChange={(val: string) =>
+            handleChange(val, `${keyValue}.pattern`)
+          }
+          value={rule.pattern}
           valueOptions={[]}
           placeHolder={"Enter pattern to evaluate"}
           length={200}
@@ -49,21 +54,20 @@ function BashCommandOutput({ rule, condition, conditionIndex }) {
       </div>
       <div className="flex flex-wrap gap-2 items-center">
         <Checkbox
-          id="caseSensitive"
-          isChecked={condition.caseSensitive}
+          id="case_sensitive"
+          isChecked={rule.case_sensitive}
           onChange={() => {
-            handleChange("caseSensitive", condition.caseSensitive);
+            addConditionToEdgeByIndex(
+              `${keyValue}.case_sensitive`,
+              !rule.case_sensitive,
+              edgeIndex,
+              conditionIndex,
+            );
           }}
           label="Pattern is Case Sensitive"
           isSmall={true}
         />
       </div>
-
-      <HandleTypes
-        rule={rule}
-        condition={condition}
-        conditionIndex={conditionIndex}
-      />
     </>
   );
 }
