80c33533da0d21731c07894401bf085ffa4968b4
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-18 17:04:12 +0530

    fix query

--- web/src/utils/builders/gcmMqlQuery.ts
+++ web/src/utils/builders/gcmMqlQuery.ts
@@ -6,7 +6,7 @@ export const gcmMqlQueryBuilder = () => {
     builder: [
       [
         {
-          key: Key.MQL_QUERY,
+          key: Key.QUERY,
           label: "MQL Query",
           type: InputTypes.MULTILINE,
         },
