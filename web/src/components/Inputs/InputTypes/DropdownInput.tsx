ccb1a4a4907a17b23c8923ad3836e1e244b58c96
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-18 19:03:09 +0530

    create a single dropdown title

--- web/src/components/Inputs/InputTypes/DropdownInput.tsx
+++ web/src/components/Inputs/InputTypes/DropdownInput.tsx
@@ -1,6 +1,6 @@
-import { KeyboardArrowDownRounded } from "@mui/icons-material";
 import React from "react";
 import DropdownOptions from "../../common/DropdownOptions/index.tsx";
+import DropdownTitle from "../../common/DropdownTitle/index.tsx";
 import useDropdown from "../../../hooks/useDropdown.ts";
 
 type DropdownInputType = {
@@ -14,43 +14,28 @@ type DropdownInputType = {
 };
 
 function DropdownInput({
-  label,
-  value,
   handleChange,
-  disabled,
-  error,
   options,
-  placeholder,
+  value,
+  disabled,
+  ...props
 }: DropdownInputType) {
   const { dropdownRef, handleSelect, isOpen, toggle } =
     useDropdown(handleChange);
 
+  const selectedValue = value ? options.find((e) => e.id === value)?.label : "";
+
   return (
     <div ref={dropdownRef} className="relative w-fit inline-block text-left">
-      <div
-        onClick={toggle}
-        className={`${
-          error ? "border-red-500" : ""
-        } flex items-center gap-2 justify-between w-full rounded border p-2 bg-white text-xs font-medium text-gray-700 focus:outline-none overflow-hidden cursor-pointer`}>
-        <input
-          className="w-full h-full rounded outline-none max-w-full min-w-[100px] font-medium text-ellipsis disabled:bg-transparent pointer-events-none"
-          type="text"
-          placeholder={placeholder ?? `Select ${label}`}
-          value={
-            value !== undefined && value !== ""
-              ? options.find((e) => e.id === value)?.label
-              : ""
-          }
-          onClick={() => console.log("woho")}
-          disabled={true}
-        />
-        <KeyboardArrowDownRounded
-          fontSize="small"
-          className={`${
-            isOpen ? "rotate-180" : "rotate-0"
-          } text-gray-600 !transition-all`}
-        />
-      </div>
+      <DropdownTitle
+        {...props}
+        toggle={toggle}
+        value={selectedValue}
+        showIcon={true}
+        inputDisabled={true}
+        disabled={disabled}
+        className={`!min-w-[100px] pointer-events-none`}
+      />
 
       {isOpen && !disabled && (
         <DropdownOptions options={options} handleSelect={handleSelect} />
--- web/src/components/common/TypingDropdown/index.tsx
+++ web/src/components/common/TypingDropdown/index.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import useDropdown from "../../../hooks/useDropdown.ts";
 import DropdownOptions from "../DropdownOptions/index.tsx";
+import DropdownTitle from "../DropdownTitle/index.tsx";
 
 const TypingDropdown = ({
   data,
   selected,
-  error,
   handleChange: change,
   disabled,
-  placeholder,
+  value,
+  ...props
 }: any) => {
   const { dropdownRef, handleSelect, isOpen, setIsOpen, toggle } =
     useDropdown(change);
@@ -33,23 +34,14 @@ const TypingDropdown = ({
 
   return (
     <div ref={dropdownRef} className="relative w-full inline-block text-left">
-      <div
-        style={{
-          backgroundColor: disabled ? "#efefef" : "",
-        }}
-        className={`${
-          error ? "border-red-500" : ""
-        } flex flex-wrap items-center gap-2 justify-between w-full rounded border p-2 bg-white text-xs font-medium text-gray-700 focus:outline-none overflow-hidden`}>
-        <input
-          className="w-full h-full rounded outline-none max-w-full min-w-[200px] font-medium text-ellipsis"
-          type="text"
-          placeholder={placeholder}
-          value={selected}
-          onChange={handleChange}
-          onClick={toggle}
-          disabled={disabled}
-        />
-      </div>
+      <DropdownTitle
+        {...props}
+        toggle={toggle}
+        value={selected}
+        disabled={disabled}
+        inputDisabed={false}
+        onChange={handleChange}
+      />
 
       {isOpen && filteredOptions?.length > 0 && !disabled && (
         <DropdownOptions
