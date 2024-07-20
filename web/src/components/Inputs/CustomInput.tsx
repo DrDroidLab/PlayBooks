e1571989f00dd195480289c307c416e006e2c2a2
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-20 15:45:07 +0530

    task and step titles

--- web/src/components/Inputs/CustomInput.tsx
+++ web/src/components/Inputs/CustomInput.tsx
@@ -8,17 +8,21 @@ import * as InputUtils from "./utils/index.ts";
 type CustomInputPropTypes = {
   suffix?: ReactNode;
   labelPosition?: LabelPosition;
+  containerClassName?: string;
 } & HandleInputRenderType;
 
 function CustomInput({
   labelPosition = LabelPosition.TOP,
+  containerClassName = "",
   ...props
 }: CustomInputPropTypes) {
   return (
     <div
       className={`${InputUtils.handleLabelPositionClassname(
         labelPosition,
-      )} ${InputUtils.handleInputTypeClassname(props.inputType)} gap-1 w-fit`}>
+      )} ${InputUtils.handleInputTypeClassname(
+        props.inputType,
+      )} ${containerClassName} gap-1 w-fit`}>
       {props.label && (
         <p className="text-xs text-gray-500">
           <b>{props.label}</b>
