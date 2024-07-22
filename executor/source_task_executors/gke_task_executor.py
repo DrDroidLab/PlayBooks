6b8fe645099c0ddd7945e13292c188ec101b840c
Mohit Goyal <mohit.goyal@drdroid.io>
2024-07-22 12:41:28 +0530

    adds changes to enable grep in kubectl

--- executor/source_task_executors/gke_task_executor.py
+++ executor/source_task_executors/gke_task_executor.py
@@ -313,7 +313,7 @@ class GkeSourceManager(PlaybookSourceManager):
                 for key, value in global_variable_set.items():
                     updated_commands = []
                     for command in commands:
-                        command = command.replace(f"{{{key}}}", value)
+                        command = command.replace(key, str(value))
                         updated_commands.append(command)
                     commands = updated_commands
             try:
