fedab992f861ed46acb932839929fbd822991000
Mohit Goyal <mohit.goyal@drdroid.io>
2024-07-23 12:13:06 +0530

    migrartes documentation task to new impl

--- executor/source_task_executors/documentation_task_executor.py
+++ executor/source_task_executors/documentation_task_executor.py
@@ -4,8 +4,11 @@ from google.protobuf.wrappers_pb2 import StringValue
 from executor.playbook_source_manager import PlaybookSourceManager
 from protos.base_pb2 import Source, TimeRange
 from protos.connectors.connector_pb2 import Connector as ConnectorProto
+from protos.literal_pb2 import LiteralType
 from protos.playbooks.playbook_commons_pb2 import PlaybookTaskResult, PlaybookTaskResultType, TextResult
 from protos.playbooks.source_task_definitions.documentation_task_pb2 import Documentation
+from protos.ui_definition_pb2 import FormField
+
 
 class DocumentationSourceManager(PlaybookSourceManager):
 
@@ -18,14 +21,26 @@ class DocumentationSourceManager(PlaybookSourceManager):
                 'model_types': [],
                 'result_type': PlaybookTaskResultType.TEXT,
                 'display_name': 'Write Markdown Documentation',
-                'category': 'Documentation'
+                'category': 'Documentation',
+                'form_fields': [
+                    FormField(key_name=StringValue(value="content"),
+                              display_name=StringValue(value="Content"),
+                              description=StringValue(value='Please enter Markdown text'),
+                              data_type=LiteralType.STRING),
+                ]
             },
             Documentation.TaskType.IFRAME: {
                 'executor': self.execute_iframe,
                 'model_types': [],
                 'result_type': PlaybookTaskResultType.TEXT,
                 'display_name': 'Embed an IFrame',
-                'category': 'Documentation'
+                'category': 'Documentation',
+                'form_fields': [
+                    FormField(key_name=StringValue(value="iframe_url"),
+                              display_name=StringValue(value="IFrame URL"),
+                              description=StringValue(value='Enter IFrame URL'),
+                              data_type=LiteralType.STRING),
+                ]
             },
         }
 
@@ -34,8 +49,8 @@ class DocumentationSourceManager(PlaybookSourceManager):
         try:
             content_output = TextResult()
             if doc_task.type == Documentation.TaskType.MARKDOWN:
-                content_output = TextResult(output = StringValue(value=doc_task.markdown.content.value))
-            return PlaybookTaskResult(type= PlaybookTaskResultType.TEXT, source=self.source, text=content_output)
+                content_output = TextResult(output=StringValue(value=doc_task.markdown.content.value))
+            return PlaybookTaskResult(type=PlaybookTaskResultType.TEXT, source=self.source, text=content_output)
         except Exception as e:
             raise Exception(f"Error while executing API call task: {e}")
 
@@ -45,7 +60,7 @@ class DocumentationSourceManager(PlaybookSourceManager):
             content_output = TextResult()
             if doc_task.type == Documentation.TaskType.IFRAME:
                 url = doc_task.iframe.iframe_url.value
-                content_output = TextResult(output = StringValue(value=f"URL: {url}"))
-            return PlaybookTaskResult(type= PlaybookTaskResultType.TEXT, source=self.source, text=content_output)
+                content_output = TextResult(output=StringValue(value=f"URL: {url}"))
+            return PlaybookTaskResult(type=PlaybookTaskResultType.TEXT, source=self.source, text=content_output)
         except Exception as e:
             raise Exception(f"Error while executing API call task: {e}")
