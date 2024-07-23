1cd21c99d2900fec1705dcef0634d588678fa8e1
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-23 18:10:58 +0530

    basic details

--- web/src/utils/playbooksData.ts
+++ web/src/utils/playbooksData.ts
@@ -1,87 +1,25 @@
 import { taskTypes } from "../constants/index.ts";
+import { playbookSelector } from "../store/features/playbook/playbookSlice.ts";
+import { store } from "../store/index.ts";
 import * as Builders from "./builders/index.ts";
 import getCurrentTask from "./getCurrentTask.ts";
 
 export const constructBuilder = (id?: string) => {
   const [task, currentStepId] = getCurrentTask(id);
+  const { supportedTaskTypes } = playbookSelector(store.getState());
+  const source = task?.source ?? "";
+  const taskType = task?.[source?.toLowerCase()].type;
+  const type = supportedTaskTypes?.find(
+    (e) => e.source === source && e.task_type === taskType,
+  );
+  console.log("type", type);
 
   if (!task) return {};
 
-  let ops: any = task?.ui_requirement.modelOptions ?? [];
-
-  switch (`${task?.source} ${task?.[task.source?.toLowerCase()].type}`) {
-    case taskTypes.CLICKHOUSE_SQL_QUERY:
-      return Builders.clickhouseBuilder(ops?.databases);
-    case taskTypes.CLOUDWATCH_LOG_GROUP:
-      return Builders.cloudwatchLogGroupBuilder(ops?.regions, task);
-    case taskTypes.CLOUDWATCH_METRIC:
-      return Builders.cloudwatchMetricBuilder(ops?.namespaces, task);
-    case taskTypes.DATADOG_SERVICE_METRIC_EXECUTION:
-      return Builders.datadogBuilder(ops?.services, task);
-    case taskTypes.GRAFANA_PROMETHEUS_DATASOURCE:
-      return Builders.grafanaDataSourceBuilder(
-        ops?.prometheus_datasources,
-        task,
-      );
-    case taskTypes.NEW_RELIC_NRQL_METRIC_EXECUTION:
-      return Builders.newRelicNRQLBuilder();
-    case taskTypes.AZURE_FILTER_LOG_EVENTS:
-      return Builders.azureLogsBuilder(ops?.workspaces, task);
-    case taskTypes.NEW_RELIC_ENTITY_APPLICATION_GOLDEN_METRIC_EXECUTION:
-      return Builders.newRelicEntityApplicationBuilder(
-        ops?.application_names,
-        task,
-      );
-    case taskTypes.NEW_RELIC_ENTITY_DASHBOARD_WIDGET_NRQL_METRIC_EXECUTION:
-      return Builders.newRelicEntityDashboardBuilder(ops?.dashboards, task);
-    case taskTypes.POSTGRES_SQL_QUERY:
-      return Builders.postgresBuilder();
-    case taskTypes.EKS_GET_DEPLOYMENTS:
-      return Builders.eksBuilder(ops?.regions, task);
-    case taskTypes.EKS_GET_EVENTS:
-      return Builders.eksBuilder(ops?.regions, task);
-    case taskTypes.EKS_GET_PODS:
-      return Builders.eksBuilder(ops?.regions, task);
-    case taskTypes.EKS_GET_SERVICES:
-      return Builders.eksBuilder(ops?.regions, task);
-    case taskTypes.EKS_KUBECTL_COMMAND:
-      return Builders.eksKubectlBuilder(ops?.regions, task);
-    case taskTypes.GKE_GET_DEPLOYMENTS:
-      return Builders.gkeBuilder(ops?.zones, task);
-    case taskTypes.GKE_GET_EVENTS:
-      return Builders.gkeBuilder(ops?.zones, task);
-    case taskTypes.GKE_GET_PODS:
-      return Builders.gkeBuilder(ops?.zones, task);
-    case taskTypes.GKE_GET_SERVICES:
-      return Builders.gkeBuilder(ops?.zones, task);
-    case taskTypes.GKE_KUBECTL_COMMAND:
-      return Builders.gkeKubectlBuilder(ops?.zones, task);
-    case taskTypes.KUBERNETES_COMMAND:
-      return Builders.kubernetesKubectlBuilder(task);
-    case taskTypes.BASH_COMMAND:
-      return Builders.bashBuilder(ops?.ssh_servers);
-    case taskTypes.DATADOG_QUERY_METRIC_EXECUTION:
-      return Builders.datadogRawQueryBuilder(task, currentStepId!);
-    case taskTypes.API_HTTP_REQUEST:
-      return Builders.apiBuilder();
-    case taskTypes.SQL_DATABASE_CONNECTION_SQL_QUERY:
-      return Builders.sqlRawQueryBuilder();
-    case taskTypes.GRAFANA_MIMIR_PROMQL_METRIC_EXECUTION:
-      return Builders.mimirBuilder();
-    case taskTypes.DOCUMENTATION_IFRAME:
-      return Builders.iframeBuilder(task);
-    case taskTypes.DOCUMENTATION_MARKDOWN:
-      return Builders.documentationBuilder();
-    case taskTypes.GRAFANA_LOKI_QUERY_LOGS:
-      return Builders.grafanaLokiBuilder();
-    case taskTypes.ELASTIC_SEARCH_QUERY_LOGS:
-      return Builders.elasticSearchBuilder(ops?.indexes);
-    case taskTypes.GCM_MQL_EXECUTION:
-      return Builders.gcmMqlQueryBuilder();
-    case taskTypes.GCM_FILTER_LOG_EVENTS:
-      return Builders.gcmLogsBuilder();
-    default:
-      break;
-  }
-  return [];
+  return type.form_fields.map((field) => ({
+    key: field.key_name,
+    label: field.display_name,
+    placeholder: field.description,
+    options: field.valid_values,
+  }));
 };
