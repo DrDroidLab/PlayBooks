147643cd8013631056727008359f91618286f5b0
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-24 14:21:23 +0530

    fix email input component

--- web/src/components/Workflows/create/notifications/EmailInputComponent.tsx
+++ web/src/components/Workflows/create/notifications/EmailInputComponent.tsx
@@ -1,8 +1,9 @@
-import React, { ReactNode } from "react";
-import ValueComponent from "../../../ValueComponent";
+import { ReactNode } from "react";
 import { useSelector } from "react-redux";
 import { currentWorkflowSelector } from "../../../../store/features/workflow/workflowSlice.ts";
 import { handleInput } from "../../utils/handleInputs.ts";
+import CustomInput from "../../../Inputs/CustomInput.tsx";
+import { InputTypes } from "../../../../types/inputs/inputTypes.ts";
 
 type EmailInputPropTypes = {
   label: string;
@@ -19,15 +20,13 @@ function EmailInputComponent({
 
   return (
     <div className="flex items-center gap-2">
-      <p className="text-xs font-medium w-12 text-gray-500">{label}:</p>
-      <ValueComponent
-        valueOptions={[]}
-        error={undefined}
-        valueType={"STRING"}
-        onValueChange={(val: string) => handleInput(key, val)}
+      <CustomInput
+        inputType={InputTypes.TEXT}
+        label={label}
         value={currentWorkflow?.[key]}
-        placeHolder={placeholder}
-        length={300}
+        handleChange={(val: string) => handleInput(key, val)}
+        placeholder={placeholder}
+        className="!w-[300px]"
       />
     </div>
   );
