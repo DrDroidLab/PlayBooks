031679a16ce58d8b557383f4c59ff7480b25cae7
Karanveer Singh Sirohi <karanveersinghsirohi@Siddarths-MacBook-Pro-2.local>
2024-07-30 12:28:32 +0530

    added cloudwatch offset

--- executor/source_managers/cloudwatch_source_manager.py
+++ executor/source_managers/cloudwatch_source_manager.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytz
 from google.protobuf.wrappers_pb2 import StringValue, DoubleValue, UInt64Value
@@ -11,6 +11,7 @@ from protos.connectors.connector_pb2 import Connector as ConnectorProto
 from protos.literal_pb2 import LiteralType, Literal
 from protos.playbooks.playbook_commons_pb2 import TimeseriesResult, LabelValuePair, PlaybookTaskResult, \
     PlaybookTaskResultType, TableResult
+from protos.playbooks.playbook_pb2 import PlaybookTask
 from protos.playbooks.source_task_definitions.cloudwatch_task_pb2 import Cloudwatch
 from protos.ui_definition_pb2 import FormField
 
@@ -104,19 +105,13 @@ class CloudwatchSourceManager(PlaybookSourceManager):
             raise e
 
     def execute_metric_execution(self, time_range: TimeRange, cloudwatch_task: Cloudwatch,
-                                 cloudwatch_connector: ConnectorProto) -> PlaybookTaskResult:
+                                 cloudwatch_connector: ConnectorProto,
+                                 execution_configuration: PlaybookTask.ExecutionConfiguration = None) -> PlaybookTaskResult:
         try:
             if not cloudwatch_connector:
                 raise Exception("Task execution Failed:: No Cloudwatch source found")
 
-            task_result = PlaybookTaskResult()
-
-            tr_end_time = time_range.time_lt
-            end_time = datetime.utcfromtimestamp(tr_end_time)
-            tr_start_time = time_range.time_geq
-            start_time = datetime.utcfromtimestamp(tr_start_time)
             period = 300
-
             task = cloudwatch_task.metric_execution
             region = task.region.value
             metric_name = task.metric_name.value
@@ -126,57 +121,96 @@ class CloudwatchSourceManager(PlaybookSourceManager):
             if task.statistic and task.statistic.value in ['Average', 'Sum', 'SampleCount', 'Maximum', 'Minimum']:
                 statistic = [task.statistic.value]
                 requested_statistic = task.statistic.value
-            task_dimensions = task.dimensions
-            dimensions = []
-            for td in task_dimensions:
-                dimensions.append({'Name': td.name.value, 'Value': td.value.value})
+            dimensions = [{'Name': td.name.value, 'Value': td.value.value} for td in task.dimensions]
 
             cloudwatch_boto3_processor = self.get_connector_processor(cloudwatch_connector, client_type='cloudwatch',
                                                                       region=region)
 
-            print(
-                "Playbook Task Downstream Request: Type -> {}, Account -> {}, Region -> {}, Namespace -> {}, Metric -> {}, Start_Time "
-                "-> {}, End_Time -> {}, Period -> {}, Statistic -> {}, Dimensions -> {}".format(
-                    "Cloudwatch_Metrics", cloudwatch_connector.account_id.value, region, namespace, metric_name,
-                    start_time,
-                    end_time, period, statistic, dimensions), flush=True)
+            labeled_metric_timeseries = []
 
-            response = cloudwatch_boto3_processor.cloudwatch_get_metric_statistics(namespace, metric_name,
-                                                                                   start_time, end_time,
-                                                                                   period, statistic, dimensions)
-            if not response or not response['Datapoints']:
-                raise Exception("No data returned from Cloudwatch")
-            response_datapoints = response['Datapoints']
-            if len(response_datapoints) > 0:
-                metric_unit = response_datapoints[0]['Unit']
-            else:
-                metric_unit = ''
+            print(f"Execution Configuration: {execution_configuration}")
 
-            metric_datapoints: [TimeseriesResult.LabeledMetricTimeseries.Datapoint] = []
-            for item in response_datapoints:
-                utc_timestamp = str(item['Timestamp'])
-                utc_datetime = datetime.fromisoformat(utc_timestamp)
-                utc_datetime = utc_datetime.replace(tzinfo=pytz.UTC)
-                val = item[requested_statistic]
-                datapoint = TimeseriesResult.LabeledMetricTimeseries.Datapoint(
-                    timestamp=int(utc_datetime.timestamp() * 1000), value=DoubleValue(value=val))
-                metric_datapoints.append(datapoint)
+            # Always get current time values
+            start_time = datetime.utcfromtimestamp(time_range.time_geq)
+            end_time = datetime.utcfromtimestamp(time_range.time_lt)
 
-            labeled_metric_timeseries = [TimeseriesResult.LabeledMetricTimeseries(
-                metric_label_values=[
-                    LabelValuePair(name=StringValue(value='namespace'), value=StringValue(value=namespace)),
-                    LabelValuePair(name=StringValue(value='statistic'),
-                                   value=StringValue(value=requested_statistic)),
-                ],
-                unit=StringValue(value=metric_unit),
-                datapoints=metric_datapoints
-            )]
+            current_response = cloudwatch_boto3_processor.cloudwatch_get_metric_statistics(
+                namespace, metric_name, start_time, end_time, period, statistic, dimensions
+            )
+            if not current_response or not current_response['Datapoints']:
+                raise Exception("No data returned from Cloudwatch for current time range")
+
+            current_metric_datapoints = [
+                TimeseriesResult.LabeledMetricTimeseries.Datapoint(
+                    timestamp=int(
+                        datetime.fromisoformat(str(item['Timestamp'])).replace(tzinfo=pytz.UTC).timestamp() * 1000),
+                    value=DoubleValue(value=item[requested_statistic])
+                ) for item in current_response['Datapoints']
+            ]
+
+            metric_unit = current_response['Datapoints'][0]['Unit'] if current_response['Datapoints'] else ''
+
+            labeled_metric_timeseries.append(
+                TimeseriesResult.LabeledMetricTimeseries(
+                    metric_label_values=[
+                        LabelValuePair(name=StringValue(value='namespace'), value=StringValue(value=namespace)),
+                        LabelValuePair(name=StringValue(value='statistic'),
+                                       value=StringValue(value=requested_statistic)),
+                        LabelValuePair(name=StringValue(value='offset_seconds'),
+                                       value=StringValue(value='0')),
+                    ],
+                    unit=StringValue(value=metric_unit),
+                    datapoints=current_metric_datapoints
+                )
+            )
+
+            # Get offset values if specified
+            if execution_configuration and execution_configuration.timeseries_offset:
+                print(f"Timeseries Offset: {execution_configuration.timeseries_offset}")
+
+                offsets = [offset.value for offset in execution_configuration.timeseries_offset]
+                for offset in offsets:
+                    # Use offset directly as seconds
+                    adjusted_start_time = start_time - timedelta(seconds=offset)
+                    adjusted_end_time = end_time - timedelta(seconds=offset)
+
+                    response = cloudwatch_boto3_processor.cloudwatch_get_metric_statistics(
+                        namespace, metric_name, adjusted_start_time, adjusted_end_time, period, statistic, dimensions
+                    )
+                    if not response or not response['Datapoints']:
+                        print(f"No data returned from Cloudwatch for offset {offset} seconds")
+                        continue
+
+                    metric_datapoints = [
+                        TimeseriesResult.LabeledMetricTimeseries.Datapoint(
+                            timestamp=int(datetime.fromisoformat(str(item['Timestamp'])).replace(
+                                tzinfo=pytz.UTC).timestamp() * 1000),
+                            value=DoubleValue(value=item[requested_statistic])
+                        ) for item in response['Datapoints']
+                    ]
+
+                    labeled_metric_timeseries.append(
+                        TimeseriesResult.LabeledMetricTimeseries(
+                            metric_label_values=[
+                                LabelValuePair(name=StringValue(value='namespace'), value=StringValue(value=namespace)),
+                                LabelValuePair(name=StringValue(value='statistic'),
+                                               value=StringValue(value=requested_statistic)),
+                                LabelValuePair(name=StringValue(value='offset_seconds'),
+                                               value=StringValue(value=str(offset))),
+                            ],
+                            unit=StringValue(value=metric_unit),
+                            datapoints=metric_datapoints
+                        )
+                    )
+
             metric_metadata = f"{namespace} for region {region} "
             for i in dimensions:
                 metric_metadata += f"{i['Name']}:{i['Value']},  "
-            timeseries_result = TimeseriesResult(metric_expression=StringValue(value=metric_name),
-                                                 metric_name=StringValue(value=metric_metadata),
-                                                 labeled_metric_timeseries=labeled_metric_timeseries)
+            timeseries_result = TimeseriesResult(
+                metric_expression=StringValue(value=metric_name),
+                metric_name=StringValue(value=metric_metadata),
+                labeled_metric_timeseries=labeled_metric_timeseries
+            )
 
             task_result = PlaybookTaskResult(type=PlaybookTaskResultType.TIMESERIES, timeseries=timeseries_result,
                                              source=self.source)
