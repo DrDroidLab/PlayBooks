664cd3b1b5596672db293d895ca8f64b7947adfe
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-30 17:35:32 +0530

    only show comparison on timeseries

--- web/src/components/Playbooks/task/taskConfiguration/index.tsx
+++ web/src/components/Playbooks/task/taskConfiguration/index.tsx
@@ -1,3 +1,5 @@
+import useCurrentTask from "../../../../hooks/playbooks/task/useCurrentTask";
+import { ResultTypeTypes } from "../../../../utils/conditionals/resultTypeOptions";
 import BulkTaskSelector from "./bulkTasks/BulkTaskSelector";
 import ComparisonSelector from "./comparison/ComparisonSelector";
 
@@ -6,13 +8,17 @@ type TaskConfigurationPropTypes = {
 };
 
 function TaskConfiguration({ id }: TaskConfigurationPropTypes) {
+  const [task] = useCurrentTask(id);
+
   return (
     <div>
       <div className="mt-2 text-sm text-violet-500">
         <b>Configuration</b>
       </div>
       <BulkTaskSelector id={id} />
-      <ComparisonSelector id={id} />
+      {task?.ui_requirement.resultType === ResultTypeTypes.TIMESERIES && (
+        <ComparisonSelector id={id} />
+      )}
     </div>
   );
 }
