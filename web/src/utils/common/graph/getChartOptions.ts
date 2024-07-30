ac5bada89f30a76784638b3ab960cc55dd2ae3af
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-07-30 17:22:41 +0530

    color palette util function

--- web/src/utils/common/graph/getChartOptions.ts
+++ web/src/utils/common/graph/getChartOptions.ts
@@ -1,30 +1,5 @@
 import dayjs from "dayjs";
-
-const generateColorPalette = (length: number) => {
-  const colors = [
-    "#5470C6",
-    "#91CC75",
-    "#EE6666",
-    "#FAC858",
-    "#73C0DE",
-    "#3BA272",
-    "#FC8452",
-    "#9A60B4",
-    "#EA7CCC",
-    "#61A0A8",
-    "#D48265",
-    "#749F83",
-    "#CA8622",
-    "#BDA29A",
-    "#6E7074",
-    "#546570",
-    "#C4CCD3",
-    "#F05B72",
-    "#DD6B66",
-    "#E39C47",
-  ];
-  return colors.slice(0, length);
-};
+import { generateColorPalette } from "../generateColorPalette";
 
 export const getChartOptions = (
   sortedTSData: any,
@@ -36,8 +11,6 @@ export const getChartOptions = (
   const xaxisArray = sortedTSData[0].datapoints.map((ts: any) => {
     return dayjs.unix(parseInt(ts.timestamp) / 1000).format("HH:mm");
   });
-
-  console.log("data", data);
 
   const colors = generateColorPalette(data.length);
 
