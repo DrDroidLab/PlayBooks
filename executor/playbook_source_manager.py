2da3de32a4bc0c88a6f6efb63f91b603a078af4f
Mohit Goyal <mohit.goyal@drdroid.io>
2024-07-31 13:05:54 +0530

    adds changes to store result transformer output in db

--- executor/playbook_source_manager.py
+++ executor/playbook_source_manager.py
@@ -4,6 +4,7 @@ from google.protobuf.struct_pb2 import Struct
 
 from connectors.crud.connectors_crud import get_db_connectors
 from connectors.models import integrations_connector_type_connector_keys_map
+from executor.source_processors.lambda_function_processor import LambdaFunctionProcessor
 from executor.source_processors.no_op_processor import NoOpProcessor
 from executor.source_processors.processor import Processor
 from protos.base_pb2 import TimeRange, Source
@@ -11,6 +12,7 @@ from protos.connectors.connector_pb2 import Connector as ConnectorProto
 from protos.literal_pb2 import LiteralType
 from protos.playbooks.playbook_commons_pb2 import PlaybookTaskResult
 from protos.playbooks.playbook_pb2 import PlaybookTask
+from protos.playbooks.source_task_definitions.lambda_function_task_pb2 import Lambda
 from protos.ui_definition_pb2 import FormField
 from utils.proto_utils import proto_to_dict, dict_to_proto
 
@@ -95,18 +97,31 @@ class PlaybookSourceManager:
                 connector_protos.append(dbc.unmasked_proto)
         return connector_protos
 
+    def apply_result_transformer(self, result: PlaybookTaskResult, lambda_function: Lambda.Function) -> Dict:
+        lambda_function_processor = LambdaFunctionProcessor(lambda_function.definition.value,
+                                                            lambda_function.requirements)
+        result_dict = proto_to_dict(result)
+        transformer_result = lambda_function_processor.execute(result_dict)
+        if not isinstance(transformer_result, Dict):
+            raise ValueError("Result transformer should return a dictionary")
+        return transformer_result
+
     def execute_task(self, account_id, time_range: TimeRange, global_variable_set: Dict,
                      task: PlaybookTask) -> PlaybookTaskResult:
         try:
             source_connector_proto = None
-            if task.task_connector_sources:
+            if task.task_connector_sources and len(task.task_connector_sources) > 0:
                 # TODO: Handle multiple connectors within task in future
                 task_connector_source = task.task_connector_sources[0]
+                if not task_connector_source.id.value:
+                    raise Exception("Connector ID not found in task")
+
                 connector_id = task_connector_source.id.value
                 all_active_valid_connectors = self.get_active_connectors(account_id=account_id,
                                                                          connector_id=connector_id)
-                source_connector_proto = all_active_valid_connectors[0] if len(
-                    all_active_valid_connectors) > 0 else None
+
+                source_connector_proto = all_active_valid_connectors[0] if \
+                    len(all_active_valid_connectors) > 0 else None
 
             task_dict = proto_to_dict(task)
             source = task_dict.get('source', '')
@@ -116,6 +131,7 @@ class PlaybookSourceManager:
             source_task = task_dict.get(source_str, None)
             if not source_task:
                 raise Exception(f"Source task not found in task: {source_str}")
+
             source_task_proto = dict_to_proto(source_task, self.task_proto)
             task_type = source_task_proto.type
             if task_type in self.task_type_callable_map:
@@ -123,17 +139,35 @@ class PlaybookSourceManager:
                     task_type_name = self.task_proto.TaskType.Name(task_type).lower()
                     source_type_task_def = source_task.get(task_type_name, {})
                     form_fields = self.task_type_callable_map[task_type]['form_fields']
+
+                    # Resolve global variables in source_type_task_def
                     resolved_source_type_task_def, task_local_variable_map = resolve_global_variables(
                         global_variable_set, form_fields,
                         source_type_task_def)
                     source_task[task_type_name] = resolved_source_type_task_def
                     resolved_task_def_proto = dict_to_proto(source_task, self.task_proto)
+
+                    # Execute task
                     playbook_task_result: PlaybookTaskResult = self.task_type_callable_map[task_type]['executor'](
-                        time_range,
-                        resolved_task_def_proto,
-                        source_connector_proto)
-                    playbook_task_result.task_local_variable_set.CopyFrom(
-                        dict_to_proto(task_local_variable_map, Struct))
+                        time_range, resolved_task_def_proto, source_connector_proto)
+
+                    # Set task local variables in playbook_task_result to be stored in database
+                    task_local_variable_map_proto = dict_to_proto(task_local_variable_map,
+                                                                  Struct) if task_local_variable_map else Struct()
+                    playbook_task_result.task_local_variable_set.CopyFrom(task_local_variable_map_proto)
+
+                    # Apply result transformer
+                    result_transformer_lambda_function_variable_set_proto = Struct()
+                    if task.execution_configuration.is_result_transformer_enabled.value:
+                        lambda_function = task.execution_configuration.result_transformer_lambda_function.value
+                        result_transformer_lambda_function_variable_set = self.apply_result_transformer(
+                            playbook_task_result, lambda_function)
+                        result_transformer_lambda_function_variable_set_proto = dict_to_proto(
+                            result_transformer_lambda_function_variable_set,
+                            Struct) if result_transformer_lambda_function_variable_set else Struct()
+                        playbook_task_result.result_transformer_lambda_function_variable_set.CopyFrom(
+                            result_transformer_lambda_function_variable_set_proto)
+
                     return playbook_task_result
                 except Exception as e:
                     raise Exception(f"Error while executing task for source: {source_str} with error: {e}")
