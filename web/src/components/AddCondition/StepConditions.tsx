74b39b4b0b6c788a626cea387e3e0027e068048f
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-08-01 18:36:32 +0530

    condition broken down to components

---
+++ web/src/components/AddCondition/StepConditions.tsx
@@ -0,0 +1,9 @@
+function StepConditions() {
+  return (
+    <div>
+      <p className="text-xs text-violet-500 font-semibold">Step Condition</p>
+    </div>
+  );
+}
+
+export default StepConditions;
--- web/src/components/AddCondition/index.tsx
+++ web/src/components/AddCondition/index.tsx
@@ -2,53 +2,26 @@ import useCurrentStep from "../../hooks/playbooks/step/useCurrentStep.ts";
 import { useSelector } from "react-redux";
 import { additionalStateSelector } from "../../store/features/drawers/drawersSlice.ts";
 import CustomButton from "../common/CustomButton/index.tsx";
-import { Add, Delete, ErrorOutlineRounded } from "@mui/icons-material";
+import { Add, ErrorOutlineRounded } from "@mui/icons-material";
 import useEdgeConditions from "../../hooks/playbooks/useEdgeConditions.ts";
 import { ruleOptions } from "../../utils/conditionals/ruleOptions.ts";
 import handleTaskTypeOptions from "../../utils/conditionals/handleTaskTypeOptions.ts";
-import HandleResultTypeForm from "./HandleResultTypeForm.tsx";
-import {
-  ResultTypeType,
-  ResultTypeTypes,
-} from "../../utils/conditionals/resultTypeOptions.ts";
 import { extractSource } from "../../utils/playbook/extractData.ts";
 import SavePlaybookButton from "../Buttons/SavePlaybookButton/index.tsx";
-import { currentPlaybookSelector } from "../../store/features/playbook/playbookSlice.ts";
-import handleTaskTypeLabels from "../../utils/conditionals/handleTaskTypeLabels.ts";
 import CustomInput from "../Inputs/CustomInput.tsx";
 import { InputTypes } from "../../types/inputs/inputTypes.ts";
 import useIsPrefetched from "../../hooks/playbooks/useIsPrefetched.ts";
+import Condition from "./Condition.tsx";
 
 function AddCondition() {
   const { source, id } = useSelector(additionalStateSelector);
-  const currentPlaybook = useSelector(currentPlaybookSelector);
-  const tasks = currentPlaybook?.ui_requirement.tasks ?? [];
-  const {
-    rules,
-    condition,
-    handleRule,
-    addNewRule,
-    deleteCondition,
-    handleGlobalRule,
-  } = useEdgeConditions(id);
+  const { rules, condition, addNewRule, handleGlobalRule } =
+    useEdgeConditions(id);
   const sourceId = extractSource(source);
   const [parentStep] = useCurrentStep(sourceId);
   const isPrefetched = useIsPrefetched();
 
   const taskTypeOptions = handleTaskTypeOptions(parentStep);
-
-  const handleTaskChange = (id: string, i: number) => {
-    const task = tasks?.find((task) => task.id === id);
-    if (!task) return;
-    handleRule("task.id", id, i);
-    handleRule("task.reference_id", task?.reference_id ?? "", i);
-    handleRule(
-      "type",
-      (task?.ui_requirement.resultType ??
-        ResultTypeTypes.OTHERS) as ResultTypeType,
-      i,
-    );
-  };
 
   return (
     <div className="p-2">
@@ -86,48 +59,12 @@ function AddCondition() {
       </div>
 
       {rules?.map((condition, i) => (
-        <div key={i} className="mt-2 border p-1 rounded-md">
-          <p className="text-xs text-violet-500 font-semibold">
-            Condition-{i + 1}
-          </p>
-          <div className="flex flex-col gap-2 flex-wrap">
-            <div className="flex items-center gap-1">
-              <CustomInput
-                inputType={InputTypes.DROPDOWN}
-                error={undefined}
-                options={taskTypeOptions?.map((task) => ({
-                  id: task?.id,
-                  label: handleTaskTypeLabels(task).label,
-                }))}
-                value={condition?.task?.id ?? ""}
-                placeholder={`Select Task`}
-                handleChange={(id: string) => handleTaskChange(id, i)}
-                disabled={!!isPrefetched}
-              />
-            </div>
-            <div className="flex flex-wrap gap-2">
-              <HandleResultTypeForm
-                resultType={
-                  (tasks?.find((task) => task.id === condition?.task?.id)
-                    ?.ui_requirement.resultType ??
-                    ResultTypeTypes.OTHERS) as ResultTypeType
-                }
-                condition={condition}
-                conditionIndex={i}
-              />
-            </div>
-
-            {!isPrefetched && (
-              <div className="flex gap-2 flex-wrap">
-                <CustomButton
-                  className="!text-sm !w-fit"
-                  onClick={() => deleteCondition(i)}>
-                  <Delete fontSize="inherit" />
-                </CustomButton>
-              </div>
-            )}
-          </div>
-        </div>
+        <Condition
+          key={i}
+          i={i}
+          condition={condition}
+          taskTypeOptions={taskTypeOptions}
+        />
       ))}
 
       {!isPrefetched && (
