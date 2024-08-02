43fd84cd1c33c824613b1dc734ac735f1c3ed3b5
Dipesh Mittal <dipesh@drdroid.io>
2024-08-02 14:33:05 +0530

    Modified routes for sandbox in new setup

--- web/src/routes.ts
+++ web/src/routes.ts
@@ -1,16 +1,11 @@
 import { PageKeys } from "./pageKeys";
 
 export const routes = {
-  [PageKeys.LOGIN]: "/login",
-  [PageKeys.SIGNUP]: "/signup",
-  [PageKeys.RESET_PASSWORD]: "/reset-password",
-  [PageKeys.OAUTH_CALLBACK]: "/oauth/callback/:oauthId",
   [PageKeys.PLAYBOOKS_CREATE]: "/playbooks/create",
   [PageKeys.PLAYBOOK_VIEW]: "/playbooks/:playbook_id",
   [PageKeys.PLAYBOOK_LOGS]: "/playbooks/logs/:playbook_run_id",
   [PageKeys.PLAYBOOK_EDIT]: "/playbooks/edit/:playbook_id",
   [PageKeys.HOME]: "/",
-  [PageKeys.SETTINGS]: "/settings",
   [PageKeys.PLAYBOOKS]: "/playbooks",
   [PageKeys.PLAYBOOK_EXECUTIONS_LIST]: "/playbooks/executions/list",
   [PageKeys.PLAYBOOK_EXECUTIONS]: "/playbooks/executions/:id",
@@ -25,8 +20,5 @@ export const routes = {
   [PageKeys.DATA_SOURCES]: "/data-sources",
   [PageKeys.CONNECTOR_PAGE]: "/data-sources/:connectorEnum",
   [PageKeys.CONNECTOR_PAGE_ID]: "/data-sources/:connectorEnum/:id",
-  [PageKeys.API_TOKENS]: "/settings/api-keys",
-  [PageKeys.INVITE_TEAM]: "/settings/invite-team",
-  [PageKeys.SUPPORT]: "/support",
   [PageKeys.NOT_FOUND]: "*",
 };
