e5d20404e27e71de07f89b64768f253a5189c8b8
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-08-06 18:00:48 +0530

    start alert automatically

--- web/src/components/Buttons/SaveOrUpdateDynamicAlertButton/index.tsx
+++ web/src/components/Buttons/SaveOrUpdateDynamicAlertButton/index.tsx
@@ -10,11 +10,14 @@ import {
 import handlePlaybookSavingValidations from "../../../utils/playbook/handlePlaybookSavingValidations";
 import { useUpdatePlaybookMutation } from "../../../store/features/playbook/api";
 import stateToPlaybook from "../../../utils/parser/playbook/stateToPlaybook";
+import { useStartWorkflowExecutionMutation } from "../../../store/features/workflow/api";
 
 function SaveOrUpdateDynamicAlertButton() {
   const { alert_id: alertId } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [triggerStartWorkflow, { isLoading: startExecutionLoading }] =
+    useStartWorkflowExecutionMutation();
 
   const [triggerSave, { isLoading: saveLoading }] =
     useCreateDynamicAlertMutation();
@@ -26,6 +29,7 @@ function SaveOrUpdateDynamicAlertButton() {
   const loading = saveLoading || updateLoading || updatePbLoading;
 
   const handleUpdate = async () => {
+    if (!alertId) return;
     try {
       const error = handlePlaybookSavingValidations();
       if (error) throw error;
@@ -34,6 +38,7 @@ function SaveOrUpdateDynamicAlertButton() {
       if (response.success) {
         const workflowUpdateResponse = await triggerUpdate().unwrap();
         if (workflowUpdateResponse.success) {
+          await triggerStartWorkflow(alertId).unwrap();
           navigate(routes.DYNAMIC_ALERTS);
         }
       }
@@ -47,6 +52,7 @@ function SaveOrUpdateDynamicAlertButton() {
       let response: any = {};
       response = await triggerSave().unwrap();
       if (response.success) {
+        await triggerStartWorkflow(response.workflow?.id).unwrap();
         navigate(routes.DYNAMIC_ALERTS);
       }
     } catch (e: any) {
@@ -64,7 +70,7 @@ function SaveOrUpdateDynamicAlertButton() {
 
   return (
     <CustomButton disabled={loading} className="w-fit" onClick={handleClick}>
-      {loading ? "Loading.." : alertId ? "Update" : "Save"}
+      {loading ? "Loading.." : alertId ? "Update and Start" : "Save and Start"}
     </CustomButton>
   );
 }
