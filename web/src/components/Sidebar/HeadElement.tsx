7c6ad38a4e5e275dae151663dd3ebfbdbf62f765
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2024-08-12 15:59:17 +0530

    change height

--- web/src/components/Sidebar/HeadElement.tsx
+++ web/src/components/Sidebar/HeadElement.tsx
@@ -9,9 +9,9 @@ function HeadElement() {
   return (
     <div
       className={`flex border-b border-gray-300 items-center justify-center ${
-        isOpen ? "flex-row" : "flex-col-reverse"
+        isOpen ? "flex-row h-[80px]" : "flex-col-reverse"
       }`}>
-      <div className="py-2 px-2 bg-white h-[80px] flex items-center justify-center flex-col">
+      <div className="py-2 px-2 bg-white flex items-center justify-center flex-col">
         <Link className="hover:!bg-transparent" to="/">
           <img
             src={isOpen ? "/logo/drdroid-logo-full.png" : "/logo/logo.png"}
