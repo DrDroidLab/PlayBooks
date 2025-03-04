84b8793dd656987fc2313c9b245af4f5af014e25
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2025-03-04 14:39:21 +0530

    minor styling improvement for buttons

--- web/src/components/common/CustomButton/index.tsx
+++ web/src/components/common/CustomButton/index.tsx
@@ -15,7 +15,7 @@ function CustomButton({
   return (
     <button
       onClick={onClick}
-      className={`${className} flex text-center gap-1 items-center text-xs bg-white hover:bg-violet-500 text-violet-500 hover:text-white rounded p-1 border border-violet-500 shrink-0 transition-all`}
+      className={`${className} flex text-center gap-1 items-center text-xs bg-white hover:bg-violet-500 text-violet-500 hover:text-white rounded p-1 border shrink-0 transition-all`}
       {...props}>
       {children}
     </button>
