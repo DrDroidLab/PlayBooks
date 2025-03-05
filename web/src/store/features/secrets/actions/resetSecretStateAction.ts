c0a08d766b5b54a1bb7d8f1f8c21b9d3ab306d9d
jayeshsadhwani99 <jayeshsadhwani99@gmail.com>
2025-03-05 14:06:50 +0530

    fix build issues

--- web/src/store/features/secrets/actions/resetSecretStateAction.ts
+++ web/src/store/features/secrets/actions/resetSecretStateAction.ts
@@ -1,8 +1,5 @@
 import { secretsInitialState, SecretsInitialState } from "../initialState";
 
 export const resetSecretStateAction = (state: SecretsInitialState) => {
-  state.name = secretsInitialState.name;
-  state.description = secretsInitialState.name;
-  state.options = secretsInitialState.options;
-  state.type = secretsInitialState.type;
+  Object.assign(state, secretsInitialState);
 };
